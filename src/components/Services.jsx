7d9cf341afe2d9f6ce43f3c6a9925125f0f044ff
farhad <farhadandproject@gmail.com>
2022-06-17 13:43:32 +0500

    services component added

---
+++ src/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled  from "styled-components";
+export default function Services() {
+  return (
+    <Section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, vero. Quas non labore modi velit, deserunt maxime id hic obcaecati! Est praesentium iure non harum quidem facere rerum exercitationem cupiditate!</p>
+    </Section>
+  )
+}
+const Section = styled.section`
+    padding: 2rem;
+    margin: 5rem 9rem;
+    line-height: 1rem;
+    font-size: 1.1rem;
+    background-color: aqua;
+    color: black;
+`;
